d0ba86702682bbbdaee590a3c63216eeaf7f1b54
danutibe07 <danielutibe07@gmail.com>
2023-04-09 22:32:54 +0100

    defined slice of state for books and categories

---
+++ src/redux/books/BookSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  books: [],
+};
+
+const bookSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, { payload }) => {
+      state.books.push({ title: payload.title, author: payload.author });
+    },
+    removeBook: (state, { payload }) => {
+      state.books.filter((book) => book.id !== payload.id);
+    },
+  },
+});
+
+export const { addBook, removeBook } = bookSlice.actions;
+export default bookSlice.reducer;
