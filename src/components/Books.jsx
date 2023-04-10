dc35124870fa987cc55f94655dc3cf1a176ff910
danutibe07 <danielutibe07@gmail.com>
2023-04-11 00:31:53 +0100

    added useEffect which includes a loader and a success states

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
 import AddBook from './AddBook';
+import { getBooks } from '../redux/books/BookSlice';
 
 const Books = () => {
-  const { books } = useSelector((store) => store.book);
+  const {
+    books, isLoading, error, ifSuccess,
+  } = useSelector((store) => store.book);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch, ifSuccess]);
+  if (isLoading) {
+    return (
+      <div>loader</div>
+    );
+  }
+  if (error) return <h2>An error has occured</h2>;
+  if (books.length === 0) return <h2>there are no books</h2>;
   return (
     <div>
       <ul>
